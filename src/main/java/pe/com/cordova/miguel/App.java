9156dfe7b6ae233b97765ef9fabd77d2b34f3b99
miguelcordova <miguelcordovar@gmail.com>
2014-09-16 10:08:14 -0500

    Importing the git-example

---
+++ src/main/java/pe/com/cordova/miguel/App.java
@@ -0,0 +1,13 @@
+package pe.com.cordova.miguel;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
